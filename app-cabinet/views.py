e9aa543225d01cde59242a34456e03db79d9c07e
dimadima5182 <dimadima5182@gmail.com>
2021-04-30 18:08:57 +0300

    add message view and master request create view

--- app-cabinet/views.py
+++ app-cabinet/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from _db import models
+from . import forms
 
 
 def index_view(request):
-    return render(request, 'cabinet/index.html')
+    user = models.User.objects.filter(id=1)[0]
+    houses = models.House.objects.filter(user=user)
+    apartments = models.Apartment.objects.filter(floor__section__house__user=user)
+    return render(request, 'cabinet/index.html', {'user': user,
+                                                  'houses': houses,
+                                                  'apartments': apartments})
 
 
 def login_view(request):
@@ -34,11 +40,19 @@ def tariffs_view_view(request):
     return render(request, 'cabinet/tariffs/view.html')
 
 
+def messages_index(request):
+    user = models.User.objects.filter(id=1)[0]
+    houses = models.House.objects.filter(user=user)
+    apartments = models.Apartment.objects.filter(floor__section__house__user=user)
+    messages = models.Message.objects.filter(destination_id=user)[::-1]
+    return render(request, 'cabinet/messages/index.html', {'user': user,
+                                                           'houses': houses,
+                                                           'apartments': apartments,
+                                                           'messages': messages
+                                                           })
+
+
 def messages_view(request):
-    return render(request, 'cabinet/messages/index.html')
-
-
-def messages_view_view(request):
     return render(request, 'cabinet/messages/view.html')
 
 
@@ -51,11 +65,25 @@ def messages_delete_view(request):
 
 
 def master_request_view(request):
+
     return render(request, 'cabinet/master-request/index.html')
 
 
 def master_request_create_view(request):
-    return render(request, 'cabinet/master-request/create.html')
+    user = models.User.objects.filter(id=1)[0]
+    houses = models.House.objects.filter(user=user)
+    apartments = models.Apartment.objects.filter(floor__section__house__user=user)
+    form = forms.MasterRequestForm(request.POST)
+    form.owner = 1
+    alerts = []
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            alerts.append('Запись была успешно добавлена!')
+    return render(request, 'cabinet/master-request/create.html', {'form': form,
+                                                                  'user': user,
+                                                                  'houses': houses,
+                                                                  'apartments': apartments})
 
 
 def master_request_delete_view(request):
