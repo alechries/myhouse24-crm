468cafaa29d9c48b6547df0fba1a1c4c77474b64
Alex <sanowese@gmail.com>
2021-05-12 09:22:11 +0300

    API

---
+++ _api/views.py
@@ -0,0 +1,18 @@
+from _db import models
+from . import serializers
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+
+
+class HouseList(generics.ListAPIView):
+    model = models.House
+    queryset = models.House.objects.all()
+    serializer_class = serializers.HouseSerializer
+
+
+class HouseDetail(generics.RetrieveAPIView):
+    model = models.House
+    queryset = models.House.objects.all()
+    serializer_class = serializers.HouseSerializer
